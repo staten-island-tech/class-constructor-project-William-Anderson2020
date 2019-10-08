2dc7a053622c4f107d63bed990c59e73bc9aa4fe
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-10-08 14:11:27 -0400

    ?

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 //Start ref. framework
+
+// Model
+    //Game Class
+    class Game{
+        constructor(name, characterArray){
+            name = this.name;
+            characterArray = this.characterArray;
+        }
+    }
+
+    //Character Class
+    class Character{
+        constructor(name, weapon, game, hp, atk, portrait){
+            name = this.name;
+            weapon = this.weapon;
+            game = this.game;
+            hp = this.hp;
+            atk = this.atk;
+            portrait = this.portrait;
+        }
+    }
+
+// View
+
+
+
+
+// Controller
+    // Event Handler
+
+    // Take user inputs
+        //Names
+        //1p 2p
+        //Game
+        //Character
+
+    // Instanciate players
+
+    // Calculate winner
+
+    // Instanciate ui
