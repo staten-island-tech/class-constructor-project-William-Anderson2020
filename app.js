72f5628346f402d1764a3791f5085971d641ac7a
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-10-08 14:47:22 -0400

    Start array selection js
    finished form html

--- app.js
+++ app.js
@@ -4,25 +4,37 @@
     //Game Class
     class Game{
         constructor(name, characterArray){
-            name = this.name;
-            characterArray = this.characterArray;
+            this.name = name;
+            this.characterArray = characterArray;
         }
     }
 
     //Character Class
     class Character{
         constructor(name, weapon, game, hp, atk, portrait){
-            name = this.name;
-            weapon = this.weapon;
-            game = this.game;
-            hp = this.hp;
-            atk = this.atk;
-            portrait = this.portrait;
+            this.name = name;
+            this.weapon = weapon;
+            this.game = game;
+            this.hp = hp;
+            this.atk = atk;
+            this.portrait = portrait;
         }
     }
 
 // View
 
+
+function displayCharacterChoices(e){
+    let playerInput = document.querySelector('#player_one_game');
+    if 
+}
+
+class UI{
+    constructor(playerOneCharacter, playerTwoCharacter){
+        this.playerOneCharacter = playerOneCharacter;
+        this.playerTwoCharacter = playerTwoCharacter;
+    }
+}
 
 
 
