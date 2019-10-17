2dbb335761c85fb5916ff161d54181a5974a3088
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-10-17 11:31:15 -0400

    Update secondary.js

--- secondary.js
+++ secondary.js
@@ -9,7 +9,7 @@ class Model{
 }
 
 const UI = new class UI{
-    displayCharacter(e) { //e for event
+    displayCharacter(e) {
         let userInput = new Model(document.getElementById('alias').value, document.getElementById('first_name').value, document.getElementById('last_name').value, document.getElementById('weapon').value, document.getElementById('image').value);
 
         let html = '<div data-aos="flip-up"><div class="display_character"><div class="display_alias display_block">%alias%</div><div class="display_first_name display_block">%firstName%</div><div class="display_last_name display_block">%lastName%</div><div class="display_weapon display_block">%weapon%</div><div class="display_image display_block"><img src="%url%" alt=""></div><div class="display_remove display_block"><p class="display_remove">Remove &#10006;</p></div></div></div>'
@@ -57,11 +57,9 @@ const UI = new class UI{
 
 
 
-const Controller = new class Controller{
-    init(){
-        document.querySelector('#character_form').addEventListener('submit', UI.displayCharacter);
-        document.querySelector('.display').addEventListener('click', UI.removeCharacter);
-    }
+//Controller
+function init(){
+    document.querySelector('#character_form').addEventListener('submit', UI.displayCharacter);
+     document.querySelector('.display').addEventListener('click', UI.removeCharacter);
 }
-
-Controller.init();
+init();
